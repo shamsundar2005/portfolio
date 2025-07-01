546513ba2797faade700ec55cc36573d302d9098
shamsundar2005 <shamsundarak2005@gmail.com>
2025-07-01 15:56:25 +0530

    3 deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,12 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/portfolio/',
   server: {
     host: "::",
     port: 8080,
   },
-  base:"/portfolio/",
+ 
   plugins: [
 
     react(),
